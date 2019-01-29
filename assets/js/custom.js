54014d5ada37be8e735abeba65feddb5895bbba9
Sohee Im <imsophia0313@gmail.com>
2019-01-29 15:06:33 +1300

    login slicing

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+
+export function showRecover(){ 
+    return alert("hhi");
+    // const element = document.getElementById("leftSidebar");
+    // const openBtn = document.getElementById("menuBtn");
+    
+    // openBtn.classList.toggle("ti-menu");
+    
+    // if(element.style.display == 'block')
+    // element.style.display = 'none';
+    // else
+    // element.style.display = 'block';
+            };
