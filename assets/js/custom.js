3fc4bf7f7dc7c58442bfb25368fa923c8f7f82a7
Sohee Im <imsophia0313@gmail.com>
2019-01-31 16:18:54 +1300

    morrisjs change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,15 @@
+/************************************
+        toggle-sidebar
+************************************/
+function toggleSidebar(){
+    const element = document.getElementById("leftSidebar");
+    const openBtn = document.getElementById("menuBtn");
 
-export function showRecover(){ 
-    return alert("hhi");
-    // const element = document.getElementById("leftSidebar");
-    // const openBtn = document.getElementById("menuBtn");
-    
-    // openBtn.classList.toggle("ti-menu");
-    
-    // if(element.style.display == 'block')
-    // element.style.display = 'none';
-    // else
-    // element.style.display = 'block';
-            };
+    openBtn.classList.toggle("ti-menu");
+        
+    if(element.style.display == 'block'){
+        element.style.display = 'none';
+    }else{
+        element.style.display = 'block';
+    }
+}
