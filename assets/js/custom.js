0448f1430984b693c74f0d9d64d5b3c00b6364e7
Sohee Im <imsophia0313@gmail.com>
2019-01-31 18:59:47 +1300

    add toast

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,19 @@ function toggleSidebar(){
         element.style.display = 'block';
     }
 }
+/************************************
+            toast
+************************************/
+$(document).ready(function(){
+    $.toast({ 
+        heading: "Welcome to Elite admin",
+        text: "Use the predefined ones, or specify a custom position object.",
+        bgColor: "#1FC0C7",
+        position: "top-right",
+        loader: true,
+        loaderBg: '#ff6849',
+        hideAfter: 3000,
+        icon:'info',
+        allowToastClose: true
+    });
+})
