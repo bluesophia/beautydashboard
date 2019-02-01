2a59702724ddd9fab3c821c9220727c6fafcebd2
bluesophia <imsophia0313@gmail.com>
2019-02-02 10:20:15 +1300

    add switch body theme-color javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,8 +5,10 @@ function toggleLeftSidebar(){
     const element = document.getElementById("leftSidebar");
     const openBtn = document.getElementById("menuBtn");
 
+    // toggle classname 'ti-menu' on menu icon
     openBtn.classList.toggle("ti-menu");
-        
+
+    // if display 'block' => change to 'none' & inverse 
     if(element.style.display == 'block'){
         element.style.display = 'none';
         
@@ -15,6 +17,8 @@ function toggleLeftSidebar(){
         element.style.transition = "all 0.3s ease-in-out";
     }
 }
+
+
 /************************************
         toggle-right-sidebar
 ************************************/
@@ -27,26 +31,47 @@ function closeRightSidebar(){
     const closeBtn = document.getElementById("rightsidebar");
     closeBtn.classList.remove("shw-rside");
 }
-/************************************
-        default-theme
-************************************/
+
+
+/**************************************************************
+            default-theme-checkbox & body-color-change
+****************************************************************/
 document.addEventListener("DOMContentLoaded", function(event) {     
+    // get links from a tag
     const links = document.querySelectorAll("#themecolors a")
-        
+    const bodyTag = document.querySelector("body")
+    
+        // call link that I clicked
         for (var i = 0; i < links.length; i++) {
             links[i].onclick = function(){
-                atag = document.querySelectorAll(".working")
-                // remove
-                atag.forEach(link => {
+                aTag = document.querySelectorAll(".working")
+                // take all link's list and remove classes 
+                aTag.forEach(link => {
+                    // remove class 'working
                     link.classList.remove("working");
+                    
+                    // remove 'skin' certain string class 
+                    const prefix = "skin";
+                    const regx = new RegExp('\\b' + prefix + '.*?\\b', 'g' )
+                    console.log(regx);
+                    bodyTag.className = bodyTag.classList.remove(regx);
+                    console.log(bodyTag);
                 })
+                // add class to link what I clicked
                 this.classList.add('working');
+                    // get data-skin
+                // const attr = this.getAttribute('data-skin')
+                const dataSkinColor = this.dataset.skin
+                    // add data-skin to body's class
+                bodyTag.classList.add(dataSkinColor);
+                bodyTag.classList.add("fixed-layout", "mini-sidebar");
             }
         }
   });
     
+
 /************************************
-            toast
+                toast
 ************************************/
 $(document).ready(function(){
     $.toast({ 
